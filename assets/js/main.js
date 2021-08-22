99bf3dc9b8173753713eb9de3f52731848a1b46d
jerryaugusto <jerryaugustodev@gmail.com>
2021-08-21 22:42:24 -0400

    Finished the 'background header' and 'scroll up button' features.

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,3 +147,25 @@ function scrollActive() {
   });
 }
 window.addEventListener('scroll', scrollActive);
+
+/**
+ * Change background header.
+ */
+function scrollHeader() {
+  const nav = document.getElementById('header');
+
+  if (this.scrollY >= 80) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+/**
+ * Show scroll.
+ */
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
