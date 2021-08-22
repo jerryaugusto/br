b219ee5123cd2914de3ad9e230ecdfba9b01278f
jerryaugusto <jerryaugustodev@gmail.com>
2021-08-21 22:17:09 -0400

    Finished the 'footer' section and 'scroll sections active link' feature.

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,29 @@ let swiper = new Swiper('.portfolio__container', {
     clickable: true,
   },
 });
+
+/**
+ * Scroll sections active link.
+ */
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
